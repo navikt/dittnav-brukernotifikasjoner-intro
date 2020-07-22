2a9af1626985ebea9637be865822ccec8f8aed80
sasoria <sergio.alberto.arevalo.soria@nav.no>
2020-07-22 11:26:28 +0200

    Updates

---
+++ precache-manifest.cfe54f1e8b2d45b563eeaf7b2635f252.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce714cbc53db2124573fdd37f94e3e1",
+    "url": "/dittnav-brukernotifikasjoner-intro/index.html"
+  },
+  {
+    "revision": "d22d87fab4f985c9d24a",
+    "url": "/dittnav-brukernotifikasjoner-intro/static/css/main.7a75ded6.chunk.css"
+  },
+  {
+    "revision": "8fc6e57604930d7af24c",
+    "url": "/dittnav-brukernotifikasjoner-intro/static/js/2.831b6757.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/dittnav-brukernotifikasjoner-intro/static/js/2.831b6757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d22d87fab4f985c9d24a",
+    "url": "/dittnav-brukernotifikasjoner-intro/static/js/main.3bba10de.chunk.js"
+  },
+  {
+    "revision": "6d6c7358357cb625fe75",
+    "url": "/dittnav-brukernotifikasjoner-intro/static/js/runtime-main.ae5efd43.js"
+  },
+  {
+    "revision": "5a36130dad0ad61b80150089c0dab77b",
+    "url": "/dittnav-brukernotifikasjoner-intro/static/media/index.5a36130d.less"
+  }
+]);
