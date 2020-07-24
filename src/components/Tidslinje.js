7de2d085364ca2e36efe293122b8e1175083edd0
sasoria <sergio.alberto.arevalo.soria@nav.no>
2020-07-24 14:25:09 +0200

    🐛 FIX: Visning av tekst

--- src/components/Tidslinje.js
+++ src/components/Tidslinje.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Normaltekst, Systemtittel } from "nav-frontend-typografi";
+import {Normaltekst, Systemtittel} from "nav-frontend-typografi";
 import Layout from "./styled/Layout";
 import Innhold from "./styled/Innhold";
+import Ingress from "./styled/Ingress";
 
 const Tidslinje = () => {
     return (
@@ -12,12 +13,16 @@ const Tidslinje = () => {
                     Komponent for å vise tidligere og kommende hendelser i en sak
                 </Systemtittel>
                 <br/>
-                <Normaltekst>
-                    Vi lager en react-komponent som basert på hendelsene (oppgaver, beskjeder og statusoppdateringer)
-                    viser bruker hva som har skjedd i saken og hva som er neste steg. Stegene vil være generiske, men
-                    tidslinjen kan tilpasses av hvert team som lager innsyn i sak for tjenestene våre.
-                </Normaltekst>
-                <img src="" alt=""/>
+                <Ingress>
+                    <Normaltekst>
+                        Vi lager en react-komponent som basert på hendelsene (oppgaver, beskjeder og
+                        statusoppdateringer)
+                        viser bruker hva som har skjedd i saken og hva som er neste steg. Stegene vil være generiske,
+                        men
+                        tidslinjen kan tilpasses av hvert team som lager innsyn i sak for tjenestene våre.
+                    </Normaltekst>
+                    <img src="" alt="" />
+                </Ingress>
             </Innhold>
         </Layout>
     )
