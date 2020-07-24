dff0043249358b2683fc3d979245543793893190
sasoria <sergio.alberto.arevalo.soria@nav.no>
2020-07-24 11:39:58 +0200

    ✨ FEATURE: Tidslinje

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Introduksjon from './components/Introduksjon';
-import './App.scss';
 import Problemer from "./components/Problemer";
 import Oversikt from "./components/Oversikt";
 import Funksjonalitet from "./components/Funksjonalitet";
 import Tidslinje from "./components/Tidslinje";
+import './App.scss';
 
 function App() {
   return (
--- src/components/Tidslinje.js
+++ src/components/Tidslinje.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import { Normaltekst, Sidetittel } from "nav-frontend-typografi";
+import { Normaltekst, Systemtittel } from "nav-frontend-typografi";
+import Layout from "./styled/Layout";
+import Innhold from "./styled/Innhold";
 
 const Tidslinje = () => {
     return (
-        <div className="header section">
-            <div className="innhold">
-                <Sidetittel>
+        <Layout backgroundColor="#C6C2BF">
+            <Innhold>
+                <Systemtittel>
                     Tidslinje: <br />
                     Komponent for å vise tidligere og kommende hendelser i en sak
-                </Sidetittel>
+                </Systemtittel>
                 <br/>
                 <Normaltekst>
                     Vi lager en react-komponent som basert på hendelsene (oppgaver, beskjeder og statusoppdateringer)
                     viser bruker hva som har skjedd i saken og hva som er neste steg. Stegene vil være generiske, men
                     tidslinjen kan tilpasses av hvert team som lager innsyn i sak for tjenestene våre.
                 </Normaltekst>
-            </div>
-        </div>
+                <img src="" alt=""/>
+            </Innhold>
+        </Layout>
     )
 };
 
